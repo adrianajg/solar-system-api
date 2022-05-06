7d8ea775214d935e84bd59a095c8b5af28bf682a
olive <olivealavine@gmail.com>
2022-05-06 10:32:10 -0500

    removed commented out code

--- app/routes/routes.py
+++ app/routes/routes.py
@@ -105,14 +105,3 @@ def update_planet_with_id(id):
     db.session.commit()
     return jsonify(planet.to_dict())
 
-# planets = [
-#     Planet(1, "Mercury", "Smallest and closest to sun", 3.7),
-#     Planet(2, "Venus", "Brightest object in the Earth's night sky", 8.8),
-#     Planet(3, "Earth", "Big blue ball, lots of water", 9.8),
-#     Planet(4, "Mars", "Baren red rock", 3.7),
-#     Planet(5, "Jupiter", "Enormous gas giant", 24.8),
-#     Planet(6, "Saturn", "Pretty rings", 10.4),
-#     Planet(7, "Uranus", "Unfortunately named", 8.9),
-#     Planet(8, "Neptune", "Smallest gas giant", 11.2),
-#     Planet(9, "Pluto", "Not really a planet", 0.6)
-# ]
